6bd1615c31978ded6411b7fe037db5bc0e4360aa
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-04-28 09:27:49 +0530

    day-1

---
+++ DAY-1/demo.java
@@ -0,0 +1,11 @@
+class demo{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("\nTernary Operator");
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum = " + max);
+
+        //System.out.println((a++)+(++b)+(--x));
+    }
+}
--- DAY-1/operators.java
+++ DAY-1/operators.java
@@ -1,4 +1,4 @@
-public class AllOperatorsDemo {
+class operators {
     public static void main(String[] args) {
 
         int a = 10, b = 5;
