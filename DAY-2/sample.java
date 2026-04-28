97a8702c0692423f43b1dafdedbacc01ebd346f3
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-04-28 14:49:31 +0530

    Day-2

--- DAY-2/sample.java
+++ DAY-2/sample.java
@@ -3,12 +3,14 @@ class sample{
         sample2 a = new sample2();
         
         a.add();
-        System.out.println(a.s);
+        System.out.println(a.a);
+        System.out.println(sample2.b);
     }
 }
 
 class sample2{
     int a = 20;
+    static int b = 30;
     void add(){
         int a=10;
         System.out.println(a);
