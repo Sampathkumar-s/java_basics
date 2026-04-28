aa9b5d5716d65eef45fe9ef95335b13cb5f93431
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-04-28 15:33:14 +0530

    3:33

--- DAY-2/sample.java
+++ DAY-2/sample.java
@@ -4,13 +4,14 @@ class sample{
         
         a.add();
         System.out.println(a.a);
-        System.out.println(sample2.b);
+        System.out.println(sample2.b); // instace variable cannot be accessed by class name, but static variable can be accessed by class name
     }
 }
 
 class sample2{
     int a = 20;
     static int b = 30;
+
     void add(){
         int a=10;
         System.out.println(a);
