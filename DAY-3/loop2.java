acb282b4458fa05ec22a7f97c03b739734536608
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-04-29 14:38:35 +0530

    while with 0 exit

---
+++ DAY-3/loop2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class loop2{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a=1;
+        while(a!=0)
+        {
+            a = s.nextInt();
+            System.out.println("user entered: "+a);
+        }
+    }
+}
