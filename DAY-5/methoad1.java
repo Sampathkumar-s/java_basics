f7f83cb21befc47d4f9d73d0b76ce65261e7f6a1
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-01 10:01:13 +0530

    Day - 5 todo program done

---
+++ DAY-5/methoad1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class methoad1{
+    public static void main(String[] args){
+        System.out.print("Enter you task: ");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        todo x = new todo();
+        x.todolist(a);
+
+    }
+}
+class todo{
+    void todolist(String a)
+    {
+        if(a.isBlank())
+        {
+            System.out.println("You not enter any task");
+            return;
+        }
+        else{
+            System.out.println("your task: "+a);
+            System.out.println("Thank you for entering the task");
+        }
+    }
+}
