41494999adf40885ddb703fcec20fe2af84331d4
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-01 13:41:53 +0530

    Constructor with 3 operation can done

--- DAY-5/cons.java
+++ DAY-5/cons.java
@@ -1,15 +1,22 @@
+
+
 class cons{
     public static void main(String[] args) {
-        constructor a = new constructor("Sampath");
+        constructors a = new constructors("Sampath"); // sending the string to the constructor 
+        constructors a2 = new constructors(a); // assign the a constructor to a2
+        constructors a3 = new constructors();  // default constructor
         System.out.println(a.name);
+        System.out.println(a2.name2);
     }
 }
 
-class constructor{
+class constructors{
     String name;
-
-    constructor(String nm ) {
+    String name2;
+    constructors(String nm ) {
         name = nm;
     }
-    
+    constructors(constructors x) {
+        name2 = x.name;
+    }
 }
