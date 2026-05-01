2dec0a09d874c0a27348f9aa1d91c1d2d0db94bf
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-01 11:10:46 +0530

    updated for the expense tracker

--- DAY-5/methoad2.java
+++ DAY-5/methoad2.java
@@ -3,17 +3,24 @@ class methoad2{
     public static void main(String[] args){
         
         String a;
-        long b;
+        long b=0;
          long c=0;
+         int x;
         do { 
+            
         System.out.print("Enter you task: ");
         Scanner sc = new Scanner(System.in);
         a = sc.nextLine();
+        
         System.out.print("Expense for the task: ");
         b = sc.nextLong();
-        c += b;
         
-        } while(b!=0);
+        c += b;
+        System.out.print("Do you want to continue(yes:1/No:0): ");
+         x = sc.nextInt();
+        
+                   
+        } while(x==1);
         System.out.println("Total Expenses: "+c);
         
     }
