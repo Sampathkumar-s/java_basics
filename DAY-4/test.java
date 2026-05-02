e46dce45f158f07fff136cdf3e915e8563f3c871
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-02 09:02:32 +0530

    Day - 4 completed

---
+++ DAY-4/test.java
@@ -0,0 +1,11 @@
+
+class test{
+    public static void main(String[] args)
+    {
+     int age = 18;
+if(age >= 18);
+System.out.println("You can vote");
+else
+System.out.println("You cannot vote");
+    }
+}
