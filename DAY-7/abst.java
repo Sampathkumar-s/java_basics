cdbd0b697dd65831fb86896d9b198058a275518d
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-03 12:12:47 +0530

    atm - check balance,deposit,withdraw

--- DAY-7/abst.java
+++ DAY-7/abst.java
@@ -3,17 +3,16 @@ class abst{
     public static void main(String[] args) {
         sample o = new sample();
         Scanner sc = new Scanner(System.in);
-       int  pin_no = 2006;
       System.out.print("Enter your pin: ");
       int pin = sc.nextInt();
         if(pin == 2006){
            while(true){
-               System.out.println("1.Check Balance\n2.Deposite\n3.Exit ");
+               System.out.println("1.Check Balance\n2.Deposite\n3.Withdraw\n4.Exit ");
                System.out.print("Enter Your choice: ");
                double choice = sc.nextDouble();
                if (choice == 1)
            {
-            System.out.println(o.getbalance());
+            System.out.println("Balance: "+o.getbalance());
            } 
            else if(choice == 2)
            {
@@ -21,32 +20,33 @@ class abst{
             double rupee = sc.nextDouble();
             o.deposit(rupee);
            }
-            else if(choice == 3)
+           else if(choice == 3)
+           {
+            System.out.print("Enter the amount for withdraw: ");
+            double rupee2 = sc.nextDouble();
+            o.withdraw(rupee2);
+           }
+            else if(choice == 4)
            {
             System.out.println("Transaction Completed");
             break;
            }
-           }         
-           
+           }          
         }
         else{
             System.out.println("Incorrect Pin!");
         }
-
-
     }
 }
-
 class sample{
     private double balance = 3000.10;
-
     double getbalance(){
-        return balance;
-        
+        return balance;  
     }
-
     void deposit(double amt){
         balance += amt;
-        System.out.println(balance);
+    }
+    void withdraw( double a){
+        balance -= a ;
     }
 }
