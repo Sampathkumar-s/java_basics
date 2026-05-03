a2bc557f6ad60726cd8162af5f65c91d1d0bb922
Sampathkumar-s <sampathkumarsks12@gmail.com>
2026-05-03 10:20:12 +0530

    Random and Math function

---
+++ DAY-7/math.java
@@ -0,0 +1,23 @@
+
+class math{
+    public static void main(String[] args) {
+
+        System.out.println("Absolute Value: "+Math.abs(5));
+        System.out.println("Max Value: "+Math.max(5,10));
+        System.out.println("Min Value: "+Math.min(5,10));
+        System.out.println("ceil Value: "+Math.ceil(2.7));
+        System.out.println("floor Value: "+Math.floor(2.7));
+        System.out.println("round Value: "+Math.round(2.7));
+         System.out.println("power Value: "+Math.pow(2,3));
+         System.out.println("square root Value: "+Math.sqrt(5));
+         System.out.println("cube root Value: "+Math.cbrt(5));
+
+         double a = 4.5555;
+         double rad = Math.toRadians(a);
+         System.out.println("Sin Value: "+Math.sin(rad));
+          System.out.println("cos Value: "+Math.cos(rad));
+           System.out.println("tan Value: "+Math.tan(rad));
+
+
+    }
+}
